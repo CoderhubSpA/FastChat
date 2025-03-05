4e4e7f1799f724117b5ab52c475c5c603d368a50
rodolfo-salgado <rodolfo.salgado10@gmail.com>
2025-03-05 17:48:33 -0300

    Migrated OpenAI API format

--- fastchat/llm_judge/common.py
+++ fastchat/llm_judge/common.py
@@ -14,6 +14,18 @@ from typing import Optional
 import openai
 import anthropic
 
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI(
+    # model=os.environ['LOCAL_LLM_MODEL_NAME'],
+    # openai_api_base=os.environ['LOCAL_LLM_API_URL'],
+    api_key=os.environ['OPENAI_API_KEY'],
+    # api_key=os.environ['LOCAL_LLM_API_KEY'],
+)
+
 from fastchat.model.model_adapter import (
     get_conversation_template,
     ANTHROPIC_MODEL_LIST,
@@ -405,23 +417,18 @@ def play_a_match_pair(match: MatchPair, output_file: str):
 
 
 def chat_completion_openai(model, conv, temperature, max_tokens, api_dict=None):
-    if api_dict is not None:
-        openai.api_base = api_dict["api_base"]
-        openai.api_key = api_dict["api_key"]
     output = API_ERROR_OUTPUT
     for _ in range(API_MAX_RETRY):
         try:
             messages = conv.to_openai_api_messages()
-            response = openai.ChatCompletion.create(
-                model=model,
-                messages=messages,
-                n=1,
-                temperature=temperature,
-                max_tokens=max_tokens,
-            )
-            output = response["choices"][0]["message"]["content"]
+            response = client.chat.completions.create(model=model,
+            messages=messages,
+            n=1,
+            temperature=temperature,
+            max_tokens=max_tokens)
+            output = response.choices[0].message.content
             break
-        except openai.error.OpenAIError as e:
+        except openai.OpenAIError as e:
             print(type(e), e)
             time.sleep(API_RETRY_SLEEP)
 
@@ -429,15 +436,6 @@ def chat_completion_openai(model, conv, temperature, max_tokens, api_dict=None):
 
 
 def chat_completion_openai_azure(model, conv, temperature, max_tokens, api_dict=None):
-    openai.api_type = "azure"
-    openai.api_version = "2023-07-01-preview"
-    if api_dict is not None:
-        openai.api_base = api_dict["api_base"]
-        openai.api_key = api_dict["api_key"]
-    else:
-        openai.api_base = os.environ["AZURE_OPENAI_ENDPOINT"]
-        openai.api_key = os.environ["AZURE_OPENAI_KEY"]
-
     if "azure-" in model:
         model = model[6:]
 
@@ -445,19 +443,17 @@ def chat_completion_openai_azure(model, conv, temperature, max_tokens, api_dict=
     for _ in range(API_MAX_RETRY):
         try:
             messages = conv.to_openai_api_messages()
-            response = openai.ChatCompletion.create(
-                engine=model,
-                messages=messages,
-                n=1,
-                temperature=temperature,
-                max_tokens=max_tokens,
-            )
-            output = response["choices"][0]["message"]["content"]
+            response = client.chat.completions.create(model=model,
+            messages=messages,
+            n=1,
+            temperature=temperature,
+            max_tokens=max_tokens)
+            output = response.choices[0].message.content
             break
-        except openai.error.OpenAIError as e:
+        except openai.OpenAIError as e:
             print(type(e), e)
             time.sleep(API_RETRY_SLEEP)
-        except openai.error.InvalidRequestError as e:
+        except openai.InvalidRequestError as e:
             print(type(e), e)
             break
         except KeyError:
--- fastchat/llm_judge/gen_api_answer.py
+++ fastchat/llm_judge/gen_api_answer.py
@@ -116,9 +116,6 @@ if __name__ == "__main__":
     parser.add_argument("--openai-api-base", type=str, default=None)
     args = parser.parse_args()
 
-    if args.openai_api_base is not None:
-        openai.api_base = args.openai_api_base
-
     question_file = f"data/{args.bench_name}/question.jsonl"
     questions = load_questions(question_file, args.question_begin, args.question_end)
 
